bb6534764e55f6d6064c48ed4cfdf5e1fc33a8cd
Richard Hsu <richardhsu.cs@gmail.com>
2014-05-30 21:41:15 +0000

    Modifying RCP folder for adding log folder

--- simulation/lib/rcp/pareto-flowSizes/run.pl
+++ simulation/lib/rcp/pareto-flowSizes/run.pl
@@ -5,20 +5,18 @@ $cap = 2.4;
 $rtt = 0.1;
 $alpha = 0.1;
 $beta = 1;
-$load = 0.8;
+$load = 0.9;
 $numbneck = 1;
 $BWdelay = ($rtt*$cap*1000000000)/(1000*8);
-@pareto_shape = (1.8);
+@pareto_shape = (1.2);
 $meanFlowSize = $BWdelay/1000;
 $init_nr_flows = 5000;
 
 for ($i = 0; $i < @pareto_shape; $i++) {
-
-`nice ns sim-rcp-pareto.tcl $sim_end $cap $rtt $load $numbneck $alpha $beta $init_nr_flows $meanFlowSize $pareto_shape[$i] > logFile`;
-`mv logFile logFile-pareto-sh$pareto_shape[$i]`;
-`mv flow.tr flow-pareto-sh$pareto_shape[$i].tr`;
-`mv queue.tr queue-pareto-sh$pareto_shape[$i].tr`;
-`mv rcp_status.tr rcp-pareto-sh$pareto_shape[$i].tr`;
-
+  `nice ns sim-rcp-pareto.tcl $sim_end $cap $rtt $load $numbneck $alpha $beta $init_nr_flows $meanFlowSize $pareto_shape[$i] > logs/logFile-pareto-sh$pareto_shape[$i]`;
+  # Move extra traces to logs folder
+  `mv flow.tr logs/flow-pareto-sh$pareto_shape[$i].tr`;
+  `mv queue.tr logs/queue-pareto-sh$pareto_shape[$i].tr`;
+  `mv rcp_status.tr logs/rcp-pareto-sh$pareto_shape[$i].tr`;
 }
 
