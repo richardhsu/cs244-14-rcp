0af15fc195554787ba745a65a8f5b5b908743f56
Lisa Y <lisasyan@gmail.com>
2014-06-01 13:03:25 -0700

    moved output messages for ps-sim into log file.

--- simulation/ps-sim.py
+++ simulation/ps-sim.py
@@ -4,7 +4,7 @@ from scipy.stats import pareto
 
 
 # Network characteristics
-num_flows = 100 
+num_flows = 1000 
 # Total number of flows in rcp simulation: 2399499
 packet_size = 1000 # bytes
 link_rate = 2.4 # Gbps
@@ -25,6 +25,9 @@ lamb = link_rate_bits*load/(mean_npkts*(packet_size)*8.0)
 # Output parameters and details
 debug_flag = False
 output_file = "lib/ps/pareto-flowSizes/logs/flowSizeVsDelay-sh" + str(shape)
+log_file_str = "lib/ps/pareto-flowsizes/logs/logFile-sh" + str(shape)
+
+log_file = open(log_file_str, 'w')
 
 class Flow(object):
     def __init__(self, arrival, length, inter_arrival):
@@ -43,11 +46,11 @@ class Flow(object):
         return cmp(self.arrival, other.arrival)
 
 def generate_flows(num_flows):
-    print "Generating flows..."
+    log_file.write("Generating flows...\n")
     all_flows = Queue.Queue()
     inter_arrivals = np.random.exponential(1.0/lamb, num_flows)
     flow_lengths = pareto.rvs(shape, scale=scale, size=num_flows)
-    print "average flow length:", sum(flow_lengths)/num_flows
+    log_file.write("average flow length:%f \n" % (sum(flow_lengths)/num_flows))
     prev_time = 0
     for i in range(num_flows):
         curr_time = prev_time + inter_arrivals[i]
@@ -55,21 +58,21 @@ def generate_flows(num_flows):
         all_flows.put(flow)
         prev_time = curr_time
         if debug_flag:
-            print "flow created: (%.8f, %d)." % (flow.arrival, flow.packet_length)
+            log_file.write("flow created: (%.8f, %d).\n" % (flow.arrival, flow.packet_length))
     max_packets = int(max(flow_lengths))
-    print "Finished flow generation."
-    print
+    log_file.write("Finished flow generation.\n")
+    log_file.write("\n")
     return inter_arrivals, all_flows, max_packets
 
 def update_flows(curr_flows, duration, curr_time):
     stop_time = curr_time + duration
-    print "Starting update in time (%.8f, %.8f) Current active flows: %d." % (curr_time, stop_time, len(curr_flows))
+    log_file.write("Starting update in time (%.8f, %.8f) Current active flows: %d.\n" % (curr_time, stop_time, len(curr_flows)))
 
     # update flow lists
     new_curr_flows = []
     done_flows = []
     if not curr_flows:
-        print "No flows to update in this interval."
+        log_file.write("No flows to update in this interval.\n")
     num_flows = len(curr_flows)
     # update how many flows each flow competed with
     for flow in curr_flows:
@@ -89,12 +92,12 @@ def update_flows(curr_flows, duration, curr_time):
                 # this flow has completed, so remove.
                 flow.fct = curr_time
                 flow.finished = True
-                print "Finished flow (arrival: %.8f, packets: %d); fct %f" % (flow.arrival, flow.packet_length, flow.fct)
+                log_file.write("Finished flow (arrival: %.8f, packets: %d); fct %f\n" % (flow.arrival, flow.packet_length, flow.fct))
         curr_time += delta
 
     for flow in curr_flows:
         if debug_flag:
-            print "status of flow (%.8f, %d): (fct: %.8f, finished: %s)" % (flow.arrival, flow.packet_length, flow.fct, flow.finished)
+            log_file.write("status of flow (%.8f, %d): (fct: %.8f, finished: %s)\n" % (flow.arrival, flow.packet_length, flow.fct, flow.finished))
         if flow.finished:
             done_flows.append(flow)
         else:
@@ -118,7 +121,7 @@ def packet_info(all_done_flows):
 
     if debug_flag:
         for packet_length in fcts_aggregate.keys():
-            print "packet length: %d" % (packet_length), fcts_aggregate[packet_length]
+            log_file.write("packet length: %d \n" % (packet_length), fcts_aggregate[packet_length])
     return fcts_aggregate
 
 def average_fct(fcts_aggregate):
@@ -137,9 +140,9 @@ def max_fct(fcts_aggregate):
 def simulate():
     """
     """
-    print "Pareto distribution with shape:", shape, "mean:", mean_npkts
-    print "Sanity check on pareto mean packets:", pareto.stats(shape, scale=scale, moments='m')
-    print "Poisson process with lambda:", lamb
+    log_file.write("Pareto distribution with shape: %d, mean: %d\n" % (shape, mean_npkts))
+    log_file.write("Sanity check on pareto mean packets: %f\n" % (pareto.stats(shape, scale=scale, moments='m')))
+    log_file.write("Poisson process with lambda: %f\n" % (lamb))
     inter_arrival, all_flows, max_packets = generate_flows(num_flows)
     curr_flows = []
     all_done_flows = []
@@ -155,27 +158,28 @@ def simulate():
         curr_time += inter_arrival
         all_done_flows += done_flows
 
-        print update_log(curr_time, init_active_count, len(done_flows), len(all_done_flows))
+        log_file.write(update_log(curr_time, init_active_count, len(done_flows), len(all_done_flows)))
+        log_file.write("\n")
 
         curr_flows.append(new_flow) # newest arriving flow
-        print arrival_log(new_flow)
-        print
+        log_file.write(arrival_log(new_flow))
+        log_file.write("\n\n")
 
     # all flows have arrived, so migrate to updating once every E[arrival] = 1/lamb.
-    print "All flows have arrived. Updating remaining flows..."
+    log_file.write("All flows have arrived. Updating remaining flows...\n")
     update_duration = 1.0/lamb
     while len(all_done_flows) != num_flows:
         init_active_count = len(curr_flows)
         curr_flows, done_flows = update_flows(curr_flows, update_duration, curr_time)
         curr_time += update_duration
         all_done_flows += done_flows
-        print update_log(curr_time, init_active_count, len(done_flows), len(all_done_flows))
-        print
+        log_file.write(update_log(curr_time, init_active_count, len(done_flows), len(all_done_flows)))
+        log_file.write("\n")
 
-    print "Finished simulation. FCT results:"
+    log_file.write("Finished simulation. FCT results:\n")
     for done_flow in all_done_flows:
         if debug_flag:
-            print "(packets: %d, fct: %.8f, bottleneck: %d flows)" % (done_flow.packet_length, done_flow.fct, done_flow.flow_bottleneck)
+            log_file.write("(packets: %d, fct: %.8f, bottleneck: %d flows)\n" % (done_flow.packet_length, done_flow.fct, done_flow.flow_bottleneck))
     fcts_aggregate = packet_info(all_done_flows)
     avg_fcts = average_fct(fcts_aggregate)
     max_fcts = max_fct(fcts_aggregate)
@@ -184,7 +188,7 @@ def simulate():
     packet_list.sort()
     ret_list = []
     for packet_length in packet_list:
-        print "Packet length: %d, average FCT: %.8f, max FCT: %.8f" % (packet_length, avg_fcts[packet_length], max_fcts[packet_length])
+        log_file.write("Packet length: %d, average FCT: %.8f, max FCT: %.8f\n" % (packet_length, avg_fcts[packet_length], max_fcts[packet_length]))
         ret_list.append((packet_length, avg_fcts[packet_length], max_fcts[packet_length]))
     return ret_list # tuples (packet_length, average fct)
 
