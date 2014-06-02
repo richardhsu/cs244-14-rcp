10aeb5d68ea8d217934d9694f2e6e7bb77cf3eac
Richard Hsu <richardhsu.cs@gmail.com>
2014-06-02 01:34:12 +0000

    Added all graph types

--- simulation/plotdata.py
+++ simulation/plotdata.py
@@ -29,7 +29,7 @@ class FlowData:
 
     for line in f:
       datum = line.rsplit(' ')
-      self.flowsizes.append(int(datum[0]))
+      self.flowsizes.append(float(datum[0]))
       self.avg_ct.append(float(datum[6]))
       self.max_ct.append(float(datum[8]))
 
@@ -53,11 +53,11 @@ def slowstart(L):
 
 class SlowStart:
 
-  def __init__(self, maxsize):
+  def __init__(self, maxsize, color):
     """ Initialize SlowStart Data
     Calculates slowstart average completion time from 0 to maxsize flows.
     """
-    self.color = 'r'
+    self.color = color
     self.marker = None
     self.linestyle = '-'
     self.label = "Slow Start"
@@ -77,11 +77,11 @@ def ps(L):
 
 class ProcessorSharing:
 
-  def __init__(self, maxsize):
+  def __init__(self, maxsize, color):
     """ Initialize Processor Sharing Data
     Calculates given processor sharing algorithm.
     """
-    self.color = 'k'
+    self.color = color
     self.marker = None
     self.linestyle = '-'
     self.label = "Processor Sharing"
@@ -90,61 +90,8 @@ class ProcessorSharing:
     self.avg_ct = map(ps, self.flowsizes)
     self.max_ct = self.avg_ct
 
-# Start the graphing
-#SHAPES = ['1.2', '2.2']
-SHAPES = ['1.2']
-
-for shape in SHAPES:
-  rcp_f = "lib/rcp/pareto-flowSizes/logs/flowSizeVsDelay-sh" + shape
-  tcp_f = "lib/tcp/pareto-flowSizes/logs/flowSizeVsDelay-sh" + shape
-  ps_f  = "lib/ps/pareto-flowSizes/logs/flowSizeVsDelay-sh" + shape
-  cps_f = "lib/custom-ps/pareto-flowSizes/logs/flowSizeVsDelay-sh" + shape
-
-  lines = [FlowData(tcp_f, '#00FF00', '.', 'TCP'),
-           SlowStart(200000),
-           FlowData(cps_f, 'c', '.', 'PS RCP Simulated'),
-           FlowData(ps_f, 'm', '.', 'Our PS Simulated'),
-           FlowData(rcp_f, 'b', '+', 'RCP'),
-           ProcessorSharing(200000)]
-
-  # Average Flow Completion Time
-  fig = plt.figure()
-  graph = fig.add_subplot(111)
-
-  for line in lines:
-    graph.plot(line.flowsizes, line.avg_ct, linestyle=line.linestyle,
-               color=line.color, marker=line.marker, label=line.label)
-
-  graph.set_yscale("log")
-  for axis in [graph.xaxis, graph.yaxis]:
-    axis.set_major_formatter(ScalarFormatter())
-  graph.axis([0, 2000, 0.1, 100])
-
-  graph.set_ylabel("Average Flow Completion Time [sec]")
-  graph.set_xlabel("flow size [pkts] (normal scale)")
-  plt.legend(loc='upper left')
-  plt.savefig("graphs/fig12-afct-" + shape + ".png", format="png")
-
-  # Average Flow Completion Time (Long flows)
-  fig = plt.figure()
-  graph = fig.add_subplot(111)
-
-  for line in lines:
-    graph.plot(line.flowsizes, line.avg_ct, linestyle=line.linestyle,
-               color=line.color, marker=line.marker, label=line.label)
-
-  graph.set_yscale("log")
-  graph.set_xscale("log")
-  for axis in [graph.xaxis, graph.yaxis]:
-    axis.set_major_formatter(ScalarFormatter())
-  graph.axis([10, 100000, 0.1, 100])
-
-  graph.set_ylabel("Average Flow Completion Time [sec]")
-  graph.set_xlabel("flow size [pkts] (log scale)")
-  plt.legend(loc='upper left')
-  plt.savefig("graphs/fig12-afct-long-" + shape + ".png", format="png")
-
-  # Max Flow Completion Time
+# Plotting
+def save_figure(lines, axis_range, ylabel, xlabel, savefn, log_y=True, log_x=False):
   fig = plt.figure()
   graph = fig.add_subplot(111)
 
@@ -152,13 +99,124 @@ for shape in SHAPES:
     graph.plot(line.flowsizes, line.max_ct, linestyle=line.linestyle,
                color=line.color, marker=line.marker, label=line.label)
 
-  graph.set_yscale("log")
+  if log_y:
+    graph.set_yscale("log")
+  if log_x:
+    graph.set_xscale("log")
+
   for axis in [graph.xaxis, graph.yaxis]:
     axis.set_major_formatter(ScalarFormatter())
-  graph.axis([0, 2000, 0.1, 100])
+  graph.axis(axis_range)
 
-  graph.set_ylabel("Max. Flow Completion Time [sec]")
-  graph.set_xlabel("flow size [pkts] (normal scale)")
+  graph.set_ylabel(ylabel)
+  graph.set_xlabel(xlabel)
   plt.legend(loc='upper left')
-  plt.savefig("graphs/fig12-maxct-" + shape + ".png", format="png")
+  plt.savefig(savefn, format="png")
+
+# Start the graphing /////////////////////////////////////////////////////////
+
+#SHAPES = ['1.2', '2.2']
+SHAPES = ['1.2']
+
+for shape in SHAPES:
+  rcp_f = "lib/rcp/pareto-flowSizes/logs/flowSizeVsDelay-sh" + shape
+  tcp_f = "lib/tcp/pareto-flowSizes/logs/flowSizeVsDelay-sh" + shape
+  cps1_f  = "lib/ps/pareto-flowSizes/logs/flowSizeVsDelay-sh" + shape
+  cps2_f = "lib/custom-ps/pareto-flowSizes/logs/flowSizeVsDelay-sh" + shape
+
+  tcp_l = FlowData(tcp_f, '#00FF00', '.', 'TCP')
+  rcp_l = FlowData(rcp_f, 'b', '+', 'RCP')
+  ss_l = SlowStart(200000, 'r')
+  ps_l = ProcessorSharing(200000, 'r')
+  custom_ps_1 = FlowData(cps1_f, 'm', '.', 'Full PS Simulated')
+  custom_ps_2 = FlowData(cps2_f, 'c', '.', 'PS Simulated')
+
+  # Graph to look like those in paper
+  lines = [tcp_l, rcp_l, ss_l, ps_l]
+  # Average Flow Completion Time
+  save_figure(lines,
+              [0, 2000, 0.1, 100],
+              "Average Flow Completion Time [sec]",
+              "flow size [pkts] (normal scale)",
+              "graphs/fig12-afct-tcp-rcp-" + shape + ".png",
+              True,
+              False)
+
+  # Average Flow Completion Time (Long flows)
+  save_figure(lines,
+              [10, 100000, 0.1, 100],
+              "Average Flow Completion Time [sec]",
+              "flow size [pkts] (log scale)",
+              "graphs/fig12-afct-long-tcp-rcp-" + shape + ".png",
+              True,
+              True)
+
+  # Max Flow Completion Time
+  save_figure(lines,
+              [0, 2000, 0.1, 100],
+              "Max. Flow Completion Time [sec]",
+              "flow size [pkts] (normal scale)",
+              "graphs/fig12-maxct-tcp-rcp-" + shape + ".png",
+              True,
+              False)
+
+  # Graphs with Processor Sharing (Custom 1)
+  ps_l.color = 'k'
+  lines = [custom_ps_1, rcp_l, ps_l]
+  # Average Flow Completion Time
+  save_figure(lines,
+              [0, 2000, 0.1, 10],
+              "Average Flow Completion Time [sec]",
+              "flow size [pkts] (normal scale)",
+              "graphs/fig12-afct-custom-ps-1-" + shape + ".png",
+              True,
+              False)
+
+  # Average Flow Completion Time (Long flows)
+  save_figure(lines,
+              [10, 100000, 0.1, 100],
+              "Average Flow Completion Time [sec]",
+              "flow size [pkts] (log scale)",
+              "graphs/fig12-afct-long-custom-ps-1-" + shape + ".png",
+              True,
+              True)
+
+  # Max Flow Completion Time
+  save_figure(lines,
+              [0, 2000, 0.1, 10],
+              "Max. Flow Completion Time [sec]",
+              "flow size [pkts] (normal scale)",
+              "graphs/fig12-maxct-custom-ps-1-" + shape + ".png",
+              True,
+              False)
+
+  # Graphs with Processor Sharing (Custom 2)
+  ps_l.color = 'k'
+  lines = [custom_ps_2, rcp_l, ps_l]
+  # Average Flow Completion Time
+  save_figure(lines,
+              [0, 2000, 0.1, 10],
+              "Average Flow Completion Time [sec]",
+              "flow size [pkts] (normal scale)",
+              "graphs/fig12-afct-custom-ps-2-" + shape + ".png",
+              True,
+              False)
+
+  # Average Flow Completion Time (Long flows)
+  save_figure(lines,
+              [10, 100000, 0.1, 100],
+              "Average Flow Completion Time [sec]",
+              "flow size [pkts] (log scale)",
+              "graphs/fig12-afct-long-custom-ps-2-" + shape + ".png",
+              True,
+              True)
+
+  # Max Flow Completion Time
+  save_figure(lines,
+              [0, 2000, 0.1, 10],
+              "Max. Flow Completion Time [sec]",
+              "flow size [pkts] (normal scale)",
+              "graphs/fig12-maxct-custom-ps-2-" + shape + ".png",
+              True,
+              False)
 
